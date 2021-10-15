2f7935a2e8cfc8d6ae8d7edd97036e4983576f16
Domenico Verde <domenico.verde@aol.com>
2021-10-15 18:27:57 +0200

    + Compact code

--- nth fibo Iterative.cpp
+++ nth fibo Iterative.cpp
@@ -2,86 +2,45 @@
 
 using namespace std;
 
-int fibo(int ctrl, int a = 0, int b= 1){
-
-    
-
+int fibo(int ctrl, int a = 0, int b= 1) {
     int temp;
+    int i = 1;
 
-    int i;
-
-    i=1;
-
-    while(i<(ctrl+1)){
-
+    while (i < (ctrl+1)) {
         i++;
 
-        
-
         temp = a;
-
         a = b;
-
         b = temp+b;
-
     }
-
     return a;
-
 }
 
 int main(){
-
-    cout<<"Enter Index Of Fibo Term You Want To Find:-\n";
-
     int ctrl;
-
-    cin>>ctrl;
-
     string kwd;
 
-    switch(ctrl){
+    cout << "Enter Index Of Fibo Term You Want To Find:-\n";
+    cin >> ctrl;
 
+    switch (ctrl) {
         case 1:
-
-        kwd = "st";
-
-        break;
-
-        
-
+            kwd = "st";
+            break;
         case 2:
-
-        kwd = "nd";
-
-        break;
-
-        
-
+            kwd = "nd";
+            break;
         case 3:
-
-        kwd = "rd";
-
-        break;
-
-        
-
+            kwd = "rd";
+            break;
         default:
-
-        kwd = "th";
-
-        break;
-
+            kwd = "th";
+            break;
     }
 
-    
-
-    
-
-    cout<<"\nThe "<<ctrl<<kwd<<" fibo term is: "<<fibo(ctrl);
+    cout << "\nThe " << ctrl << kwd << " fibo term is: " << fibo(ctrl);
 
     return 0;
-
 }
 
 //end//
--- nth fibo recursive.cpp
+++ nth fibo recursive.cpp
@@ -1,69 +1,40 @@
 #include <iostream>
+
 using namespace std;
-int fibo(int ctrl){
-    if(ctrl==0){
+
+int fibo (int ctrl) {
+    if (ctrl == 0) {
         return 0;
-
-    }
-
-    else if(ctrl == 1){
-
+    } else if (ctrl == 1) {
         return 1;
-
-    }
-
-    else{
-
+    } else {
         return (fibo(ctrl-1) + fibo(ctrl-2));
-
     }
-
-    
-
 }
 
 int main(){
-
-    cout<<"Enter Index Of Fibo Term You Want To Find:-\n";
-
     int ctrl;
-
-    cin>>ctrl;
-
     string kwd;
 
-    switch(ctrl){
+    cout << "Enter Index Of Fibo Term You Want To Find:-\n";
+    cin >> ctrl;
 
+    switch (ctrl) {
         case 1:
-
-        kwd = "st";
-
-        break;
+            kwd = "st";
+            break;
         case 2:
-        kwd = "nd";
-        break;
+            kwd = "nd";
+            break;
         case 3:
-
-        kwd = "rd";
-
-        break;
-
-        
-
+            kwd = "rd";
+            break;
         default:
-
-        kwd = "th";
-
-        break;
-
+            kwd = "th";
+            break;
     }
 
-    
-
-    
-
-    cout<<"\nThe "<<ctrl<<kwd<<" fibo term is: "<<fibo(ctrl);
+    cout << "\nThe " << ctrl << kwd << " fibo term is: " << fibo(ctrl);
 
     return 0;
-
 }
